7cb9026f1d008bee93976664c5904e109ee18145
camilo-gutierrez <camilo1523172@gmail.com>
2021-11-04 12:10:19 -0500

    make it better

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '1cf53cd51921acf7329af3f85d91d9ae'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '0903b8f4-4e26-4530-91a9-6cedf56c9b18'
+};
